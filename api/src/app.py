03cfbe5c47e454da80551308a5afd07110b5e67d
Curtis Rubeck <rubeckcurtis@gmail.com>
2024-04-12 23:10:22 -0500

    feat: docker compose

--- api/src/app.py
+++ api/src/app.py
@@ -1,7 +1,36 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello Farmers Market!</p>"
+users = [
+    {
+        "userId": "1",
+        "firstName": "curtis",
+        "lastName": "rubeck"
+    },
+    {
+        "userId": "2",
+        "firstName": "max",
+        "lastName": "lemke"
+    }
+]
+
+@app.route("/users")
+def get_users():
+    return json.dumps(users) 
+
+@app.route("/users/<id>")
+def get_user(id):
+    for user in users:
+        if user.get("userId") == id:
+            return json.dumps(f"Hello user_id {user}")
+
+def update_user(id):
+    pass
+
+def create_user():
+    pass
+
+def delete_user(id):
+    pass
